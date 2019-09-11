9a084df0cef424e81cfbecf1acae54babf398496
moisegrat1212 <45602501+moisegrat1212@users.noreply.github.com>
2019-09-11 16:25:01 -0400

    Update APIAlodigaWallet.java

--- src/com/alodiga/wallet/ws/APIAlodigaWallet.java
+++ src/com/alodiga/wallet/ws/APIAlodigaWallet.java
@@ -44,7 +44,7 @@ public class APIAlodigaWallet {
     }
     
     //coment21
-    //cambio2
+    //cambio26
     @WebMethod
     public UserHasProductResponse saveUserHasProduct(
         @WebParam(name = "userId") String userId,
