d74710881f31abe16d273e66c8fceca43f06cdac
moisegrat1212 <moisegrat12@gmail.com>
2019-10-04 09:53:06 -0400

    cambio de moises

--- build/generated-sources/ap-source-output/com/alodiga/wallet/model/Category_.java
+++ build/generated-sources/ap-source-output/com/alodiga/wallet/model/Category_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-30T15:02:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-04T09:46:28")
 @StaticMetamodel(Category.class)
 public class Category_ { 
 
