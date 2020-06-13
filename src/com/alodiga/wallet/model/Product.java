d08fcfb029b665c06ac2aee21b1a71ee39607beb
Henry Varona <henry.varona@gmail.com>
2020-06-13 09:35:37 -0400

    Cambios para adaptarse al registorunificado

--- src/com/alodiga/wallet/model/Product.java
+++ src/com/alodiga/wallet/model/Product.java
@@ -1,12 +1,5 @@
 package com.alodiga.wallet.model;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.io.Serializable;
 import java.util.Collection;
 import javax.persistence.Basic;
